a1dc376cfb8d6722944f5fd3997e087b3dae679b
KhanMarshaI <iamtaha05@gmail.com>
2025-05-18 22:29:30 +0500

    Implement random graph generation for adjacency matrix.

--- adjMatrix.cpp
+++ adjMatrix.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <random>
+#include <climits>
+#include <chrono>
 using namespace std;
 
 int** initGraph(int V) {
@@ -26,6 +29,14 @@ void deleteGraph(int** graph, int V) {
 	delete[] graph;
 }
 
+void deleteGraph(bool** graph, int V) {
+	for (int i = 0; i < V; i++) {
+		delete[] graph[i];
+	}
+
+	delete[] graph;
+}
+
 void printGraph(int** graph, int V) {
 	//print
 	for (int i = 0; i < V; i++) {
@@ -45,19 +56,94 @@ void populateGraph(int** graph, int V, int edges) {
 	}
 }
 
+int** random_graph(int nodes, int edges_limit, int seed = 0) {
+	int** graph = new int* [nodes]; //VxV matrix
+	//each idx points to an array of int (row/neighbor of current vertex)
+
+	for (int i = 0; i < nodes; i++) {
+		graph[i] = new int[nodes]; //point to an array of integers (row)
+		for (int j = 0; j < nodes; j++) {
+			graph[i][j] = -1; //sentinel value (no connection)
+		}
+	}
+
+	//for tracking generated edges
+	bool** existing = new bool*[nodes];
+	for (int i = 0; i < nodes; i++) {
+		existing[i] = new bool[nodes];
+		for (int j = 0; j < nodes; j++) {
+			existing[i][j] = false;
+		}
+	}
+
+	bool* connected = new bool[nodes];
+	for (int i = 0; i < nodes; i++) {
+		connected[i] = false;
+	}
+
+	srand(seed);
+	
+	//spanning tree
+	connected[0] = true;
+	int connected_count = 1;
+	int edge_count = 0;
+
+	while (connected_count < nodes && edge_count < edges_limit) {
+		//connected u vertex
+		int u = rand() % nodes;
+		while (!connected[u]) u = rand() % nodes;
+
+		//unconnected v vertex
+		int v = rand() % nodes;
+		while (connected[v] || u == v) v = rand() % nodes;
+
+		graph[u][v] = graph[v][u] = 0;
+		existing[u][v] = existing[v][u] = true;
+		connected[v] = true;
+
+		connected_count++;
+		edge_count++;
+	}
+
+	while (edge_count < edges_limit) {
+		int u = rand() % nodes;
+		int v = rand() % nodes;
+
+		if (u == v || existing[u][v]) continue;
+		if (u > v) swap(u, v);
+
+		graph[u][v] = graph[v][u] = 0;
+		existing[u][v] = existing[v][u] = 0;
+		edge_count++;
+	}
+
+	deleteGraph(existing, nodes);
+	delete[] connected;
+
+	return graph;
+}
+
 int main() {
 	int V;
 	cout << "Vertices? ";
 	cin >> V;
 
-	int** graph = initGraph(V);
-
 	int edges;
 	cout << "How many edges do you want to add? ";
 	cin >> edges;
 
-	populateGraph(graph, V, edges);
-	printGraph(graph, V);
+	int seed = 1051962;
+
+	auto start = chrono::high_resolution_clock::now();
+	int** graph = random_graph(V, edges, seed);
+	auto stop = chrono::high_resolution_clock::now();
+
+	auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+
+	cout << "Random generation of " << V << " vertices with seed " << seed << " took " << duration.count() << "microsec" << endl;
+
+	//populateGraph(graph, V, edges);
+	//printGraph(graph, V);
 	deleteGraph(graph, V);
 	
 	return 0;
